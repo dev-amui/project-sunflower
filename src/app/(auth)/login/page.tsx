b0dbc8beeb7d214b0f8a0595c65d38c8dfb13167
NiiAmui <samniiamui@gmail.com>
2025-09-11 12:06:25 +0000

    add animations to the change in current view for the login and register pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,9 +5,8 @@ import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Form } from '@/components/ui/form'
-import { InputFormField, OTPFormField, PasswordFormField, PhoneNumberFormField } from '@/customComponents/FormFields'
+import { OTPFormField, PhoneNumberFormField } from '@/customComponents/FormFields'
 import ButtonLoading from '@/customComponents/Button'
-import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { OtpResendTimer } from '@/customComponents/OTPResetTimer'
 import ProgressBarComponent from '@/customComponents/ProgressBarComponent'
@@ -20,82 +19,89 @@ const otpSchema = z.object({
   otp: z.string().min(1, 'OTP is required').min(4, 'OTP must be 4 digits'),
 })
 
-const page = () => {
-  const [currentView, setcurrentView] = useState<'Phone' | 'Otp'>('Phone')
+const Page = () => {
+  const [currentView, setCurrentView] = useState<'Phone' | 'Otp'>('Phone')
   const progressBarRef = useRef<any>(null)
 
   const phoneNumberForm = useForm<z.infer<typeof phoneNumberSchema>>({
     resolver: zodResolver(phoneNumberSchema),
     defaultValues: {
       phoneNumber: '',
-    }
+    },
   })
 
   const otpForm = useForm<z.infer<typeof otpSchema>>({
     resolver: zodResolver(otpSchema),
     defaultValues: {
       otp: '',
-    }
+    },
   })
 
   const handleResendOTP = () => {
     console.log('resend OTP')
   }
 
-  const handleSumbit = (data: any) => {
-    setcurrentView('Otp')
+  const handleSubmit = (data: any) => {
+    setCurrentView('Otp')
     progressBarRef?.current?.next?.()
   }
 
   return (
-    <div className='flex flex-col justify-center items-center w-[500px] mx-auto h-full'>
-      <div className="headerSection text-left  w-full">
-        {/* main header */}
-        <p className="header ">
-          Welcome Back
-        </p>
-
-        <p className="description mt-2 text-gray-500">
+    <div className="flex flex-col justify-center items-center w-[500px] mx-auto h-full">
+      {/* Header */}
+      <div className="headerSection text-left w-full">
+        <p className="header">Welcome Back</p>
+        <p className="description mt-1 text-gray-500">
           Enter your email and password to login
         </p>
       </div>
 
+      {/* Stepper */}
+      <div className="stepperProgress w-full mt-6">
+        <ProgressBarComponent steps={[1, 2]} progressBarClass="w-full" ref={progressBarRef} />
+      </div>
 
-      {/* steps reached */}
-      <ProgressBarComponent steps={[1, 2]} progressBarClass='w-full' ref={progressBarRef}/>
-
-      {currentView == 'Phone' ? <Form {...phoneNumberForm}>
-        <form className='w-full flex flex-col gap-3 mt-10' onSubmit={phoneNumberForm.handleSubmit(handleSumbit)}>
-          <div className="inputs">
-            <PhoneNumberFormField form={phoneNumberForm} name='phoneNumber' label='Phone Number' />
-          </div>
-
-          <ButtonLoading title='Submit' />
-        </form>
-        <div className="login text-sm text-right mt-4 w-full">
-          <span className='text-gray-400'>Don't have an account? Click here to</span> <Link className='text-primary' href={'/register'}>Register</Link>
-        </div>
-      </Form>
-        :
-        <Form {...phoneNumberForm}>
-          <form className='w-full flex flex-col gap-3 mt-10'>
-            <div className="inputs">
-              <OTPFormField form={otpForm} name='otp' maxLength={4} label='OTP' />
+      {/* Animated Views */}
+      <div className="relative w-full h-[250px] overflow-hidden mt-4">
+        {/* Phone View */}
+        <div
+          className={`absolute top-0 left-0 w-full transition-transform duration-500 ease-in-out
+          ${currentView === 'Phone' ? 'translate-x-0' : '-translate-x-full'}`}
+        >
+          <Form {...phoneNumberForm}>
+            <form className="w-full flex flex-col gap-3" onSubmit={phoneNumberForm.handleSubmit(handleSubmit)}>
+              <PhoneNumberFormField form={phoneNumberForm} name="phoneNumber" label="Phone Number" />
+              <ButtonLoading title="Submit" />
+            </form>
+            <div className="login text-sm text-right mt-4 w-full">
+              <span className="text-gray-400">Don't have an account? Click here to </span>
+              <Link className="text-primary" href="/register">
+                Register
+              </Link>
             </div>
+          </Form>
+        </div>
 
-            <ButtonLoading title='Submit OTP' />
-          </form>
+        {/* OTP View */}
+        <div
+          className={`absolute top-0 left-0 w-full transition-transform duration-500 ease-in-out
+          ${currentView === 'Otp' ? 'translate-x-0' : 'translate-x-full'}`}
+        >
+          <Form {...otpForm}>
+            <form className="w-full flex flex-col gap-3">
+              <OTPFormField form={otpForm} name="otp" maxLength={4} label="OTP" />
+              <ButtonLoading title="Submit OTP" />
+            </form>
+            <div className="login text-sm text-right mt-4 w-full flex justify-end items-center h-[35px]">
+              <OtpResendTimer duration={2} onResend={handleResendOTP} />
+            </div>
+          </Form>
+        </div>
+      </div>
 
-          {/* resend otp */}
-          <div className="login text-sm text-right mt-4 w-full flex justify-end items-center h-[35px]">
-            <OtpResendTimer duration={2} onResend={handleResendOTP} />
-          </div>
-        </Form>}
-
-
-      {/* footer */}
+      {/* Footer */}
       <div className="footer mt-4">
-        <p className="text-sm text-gray-500 ">
+        <p className="text-sm text-gray-500">
           By signing in or creating an account, you agree to our{' '}
           <a href="/terms" className="text-primary underline">
             Terms of Service
@@ -107,8 +113,7 @@ const page = () => {
         </p>
       </div>
     </div>
-
   )
 }
 
-export default page
+export default Page
