28a729ccbfabb65a6380783dc95773fca4398510
NiiAmui <samniiamui@gmail.com>
2025-09-19 15:50:43 +0000

    update the add event page

--- src/app/private/admin/applications/events/partials/NewEventSlider.tsx
+++ src/app/private/admin/applications/events/partials/NewEventSlider.tsx
@@ -8,11 +8,12 @@ import {
     SheetHeader,
     SheetTitle,
 } from "@/components/ui/sheet"
-import { InputFormField, SwitchFormField, TextAreaFormField } from "@/customComponents/FormFields";
+import { InputFormField, SelectFormField, SwitchFormField, TextAreaFormField } from "@/customComponents/FormFields";
 import z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form } from "@/components/ui/form";
+import IconifyIcon from "@/customComponents/IconifyIcon";
 
 
 type AddPaymentMethodProps = {
@@ -21,14 +22,7 @@ type AddPaymentMethodProps = {
 };
 
 const newEventSchema = z.object({
-    name: z.string().min(1, 'Stock Name is required'),
-    type: z.string().min(1, 'Stock type is required'),
-    expiryDate: z.string().optional(),
-    batchNumber: z.string().optional(),
-    description: z.string().optional(),
-    isActive: z.boolean().optional(),
-    quantity: z.number().min(1, 'Quantity is required'),
-    dateReceived: z.string(),
+
 });
 
 export default function NewEventSlider({ open, onOpenChange }: AddPaymentMethodProps) {
@@ -36,14 +30,6 @@ export default function NewEventSlider({ open, onOpenChange }: AddPaymentMethodP
         resolver: zodResolver(newEventSchema),
         mode: 'onChange',
         defaultValues: {
-            name: '',
-            type: '',
-            expiryDate: '',
-            batchNumber: '',
-            quantity: 0,
-            dateReceived: new Date().toISOString().split('T')[0],
-            isActive: true,
-            description: '',
         },
     });
 
@@ -51,32 +37,58 @@ export default function NewEventSlider({ open, onOpenChange }: AddPaymentMethodP
         <Sheet modal={true} open={open} onOpenChange={onOpenChange}>
             <SheetContent className="flex flex-col justify-between h-full">
                 <SheetHeader>
-                    <SheetTitle>Stock In</SheetTitle>
+                    <SheetTitle>New Event</SheetTitle>
                     <SheetDescription>
-                        Add new stock to your inventory. Ensure all details are correct before proceeding.
+                        Add all the events that you expect to happen within this
                     </SheetDescription>
                 </SheetHeader>
                 <Form {...form}>
                     <form action="" className="flex flex-col flex-1 max-h-[80vh] overflow-y-auto">
-                        <div className="grid flex-1 auto-rows-min gap-6 px-4">
+                        <div className="grid flex-1 auto-rows-min gap-4 px-4">
                             <div className="isActive flex justify-end">
                                 {/* isActive */}
-                                <SwitchFormField form={form} name='isActive' label="Active" />
+                                <SwitchFormField form={form} name='isPaid' label="Paid Event" className="" />
                             </div>
-                            {/* name */}
-                            <InputFormField form={form} name="name" label="Stock Name" placeholder="Enter stock name..." />
-                            {/* type */}
-                            <InputFormField form={form} name="type" label="Stock Type" placeholder="Enter stock type..." />
-                            {/* expiryDate */}
-                            <InputFormField form={form} name="expiryDate" label="Expiry Date" placeholder="Enter expiry date..." />
-                            {/* batchNumber */}
-                            <InputFormField form={form} name="batchNumber" label="Batch Number" placeholder="Enter batch number..." />
-                            {/* quantity */}
-                            <InputFormField form={form} name="quantity" label="Quantity" placeholder="Enter stock quantity..." />
-                            {/* dateReceived */}
-                            <InputFormField form={form} name="dateReceived" label="Date Received" type="date" placeholder="Enter date received..." />
+
+                            {/* Inoculation or Weighing Form */}
+                            <div className="imageSelection ">
+                                <label htmlFor='productImages' className=' flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50 data-[error=true]:text-destructive'>Event Banner <span className='text-red-500'>*</span></label>
+                                <div className="selectionContainer bg-[#F7F9FF] h-[100px] border border-dashed mt-2 rounded flex flex-col items-center justify-center gap-2">
+                                    {/* image icon */}
+                                    <IconifyIcon icon='mage:image-upload' fontSize={40} className='text-lg text-blue-600' />
+                                    <div className="textSection text-center">
+                                        <p className='text-sm'> <label className='text-blue-600' htmlFor='productImages'>Click here</label> to upload or drag and drop</p>
+                                        <p className='text-xs'>Maximum file size 2MB</p>
+                                        <input type='file' className='hidden' id='productImages' />
+                                    </div>
+                                </div>
+                            </div>
+                            {/* event name */}
+                            <InputFormField form={form} name="eventName" label="Event Name" placeholder="Enter event name..." />
                             {/* description */}
-                            <TextAreaFormField form={form} name="description" label="Description" placeholder="Enter more description..." />
+                            <TextAreaFormField form={form} name="type" label="Event Description" placeholder="Enter event Description..." />
+                            {/* event Date and time */}
+                            <div className="eventDateNTimes grid grid-cols-2 space-y-2 gap-x-4">
+                                <div className="eventDate col-span-2">
+                                    <InputFormField form={form} name="eventDate" label="Event Date" />
+                                </div>
+                                <InputFormField form={form} name={"startTime"} label="Start Time" type="time" />
+                                <InputFormField form={form} name={"endTime"} label="End Time" type="time" />
+                            </div>
+                            {/* event type */}
+                            <SelectFormField form={form} name="eventType" label="Event type" placeholder="" options={[{ label: 'In Person', value: 'In Person' }, { label: 'Online', value: 'Online' }, { label: 'Hybrid', value: 'Hybrid' }]} />
+                            {/* Event Address */}
+                            <InputFormField form={form} name="address" label="Event Address" placeholder="Enter event address..." />
+                            {/* event url */}
+                            <InputFormField form={form} name="eventURl" label="Online URL" type="text" placeholder="Enter Meeting url..." />
+                            {/* amount */}
+                            <InputFormField form={form} name="amount" label="Amount" placeholder="Enter amount..." type="number" />
+
+                            <div className="isActive">
+                                <p className="text-gray-500 italic">Switch if the participants of this event will be graded or not.</p>
+                                {/* isActive */}
+                                <SwitchFormField form={form} name='isGraded' label="" />
+                            </div>
                         </div>
                     </form>
                 </Form>
