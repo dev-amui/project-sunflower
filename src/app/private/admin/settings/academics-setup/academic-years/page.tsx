c74e67854aee4f167339675c4bc62f81e4842c2b
NiiAmui <samniiamui@gmail.com>
2025-10-03 21:32:41 +0000

    add grading cards

--- src/app/private/admin/settings/academics-setup/academic-years/page.tsx
+++ src/app/private/admin/settings/academics-setup/academic-years/page.tsx
@@ -39,7 +39,7 @@ const page = () => {
     return (
         <div className='AcademicYearPage bg-gray-100 p-4 lg:p-6 grid grid-cols-1 md: md:grid-cols-2 lg:grid-cols-3 gap-4'>
             {academicYears.map((el, idx) => (
-                <AcademicYearCard year={el} />
+                <AcademicYearCard year={el} key={idx} />
             ))}
 
         </div>
