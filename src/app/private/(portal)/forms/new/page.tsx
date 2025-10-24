441b7d54aceb06ce08415015ff7226cae3801f7f
NiiAmui <samniiamui@gmail.com>
2025-10-24 09:35:45 +0000

    add the application selection to the form filling stepper card

--- src/app/private/(portal)/forms/new/page.tsx
+++ src/app/private/(portal)/forms/new/page.tsx
@@ -1,55 +1,78 @@
-'use client';
+"use client";
 
-import StepperCard, { IStep } from '@/customComponents/StepperCard';
-import React, { useEffect, useState } from 'react'
-import GuardianForm from './partials/GuardianForm';
-import ChildForm from './partials/ChildForm';
-import FormPaymentForm from './partials/FormPaymentForm';
+import StepperCard, {
+  IStep,
+  StepperCardRef,
+} from "@/customComponents/StepperCard";
+import React, { useRef, useState } from "react";
+import GuardianForm from "./partials/GuardianForm";
+import ChildForm from "./partials/ChildForm";
+import FormPaymentForm from "./partials/FormPaymentForm";
+import ApplicationSelectionStep from "./partials/ApplicationSelectionStep";
 
-const FormSteps: IStep[] = [
-  {
-    title: 'Parent/Guardian Form',
-    id: 1,
-    content: <GuardianForm />,
-    // description: 'Some descriptions',
-    leftControlBtnLabel: 'Cancel 1',
-    rightControlBtnLabel: 'Submit'
-  },
-  {
-    title: 'Ward Form',
-    id: 2,
-    content: <ChildForm />,
-    // description: 'Some descriptions',
-    leftControlBtnLabel: 'Previous 2',
-    rightControlBtnLabel: 'Submit'
-  },
-  {
-    title: 'Payment Form',
-    id: 3,
-    content: <FormPaymentForm />,
-    // description: 'Some descriptions',
-    leftControlBtnLabel: 'Cancel',
-    rightControlBtnLabel: 'Submit'
-  }
-]
+const ApplicationFormPage = () => {
+  const stepperRef = useRef<StepperCardRef>(null);
 
-const page = () => {
-  const [currentStep, setcurrentStep] = useState<number>(1)
+  const [currentStep, setcurrentStep] = useState<number>(1);
 
   const handleRightButtonClick = (val: () => void) => {
-    val?.()
-  }
+    val?.();
+  };
 
   const handleLeftButtonClick = (val: () => void) => {
-    val?.()
-  }
+    val?.();
+  };
 
+  const FormSteps: IStep[] = [
+    {
+      title: "Parent/Guardian Form",
+      id: 1,
+      content: (
+        <ApplicationSelectionStep goToNextStep={stepperRef.current?.next} />
+      ),
+      // description: 'Some descriptions',
+      leftControlBtnLabel: "Cancel",
+      rightControlBtnLabel: "Submit",
+    },
+    {
+      title: "Parent/Guardian Form",
+      id: 2,
+      content: <GuardianForm />,
+      // description: 'Some descriptions',
+      leftControlBtnLabel: "Previous",
+      rightControlBtnLabel: "Submit",
+    },
+    {
+      title: "Ward Form",
+      id: 3,
+      content: <ChildForm />,
+      // description: 'Some descriptions',
+      leftControlBtnLabel: "Previous",
+      rightControlBtnLabel: "Submit",
+    },
+    {
+      title: "Payment Form",
+      id: 4,
+      content: <FormPaymentForm />,
+      // description: 'Some descriptions',
+      leftControlBtnLabel: "Cancel",
+      rightControlBtnLabel: "Submit",
+    },
+  ];
 
   return (
-    <div className='h-full py-6'>
-      <StepperCard steps={FormSteps} leftBtnClicked={handleLeftButtonClick} rightBtnClicked={handleRightButtonClick} classNames={{ cardClass: 'bg-white/40 backdrop-blur-sm' }} getCurrentStep={setcurrentStep}/>
+    <div className="h-full py-6">
+      <StepperCard
+        ref={stepperRef}
+        steps={FormSteps}
+        leftBtnClicked={handleLeftButtonClick}
+        rightBtnClicked={handleRightButtonClick}
+        classNames={{ cardClass: "bg-white/40 backdrop-blur-sm" }}
+        getCurrentStep={setcurrentStep}
+        hideActionBtns={stepperRef.current?.getCurrentStep() === 1}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default ApplicationFormPage;
