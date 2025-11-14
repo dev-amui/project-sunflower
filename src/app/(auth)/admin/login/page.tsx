7d73ae44e71a16aa97717081caecf8f8f862fa62
NiiAmui <samniiamui@gmail.com>
2025-11-14 08:16:17 +0000

    make the use client pages component page

--- src/app/(auth)/admin/login/page.tsx
+++ src/app/(auth)/admin/login/page.tsx
@@ -1,74 +1,99 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import * as z from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Form } from '@/components/ui/form'
-import { InputFormField, PasswordFormField } from '@/customComponents/FormFields'
-import ButtonLoading from '@/customComponents/Button'
-import { Button } from '@/components/ui/button'
-import { useRouter } from 'next/navigation'
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import {
+  InputFormField,
+  PasswordFormField,
+} from "@/customComponents/FormFields";
+import ButtonLoading from "@/customComponents/Button";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 const loginSchema = z.object({
-    email: z.string().min(1, 'Email or username is required'),
-    password: z.string().min(6, 'Password is required and must be atleast 6 characters')
-})
+  email: z.string().min(1, "Email or username is required"),
+  password: z
+    .string()
+    .min(6, "Password is required and must be atleast 6 characters"),
+});
 
-const page = () => {
-    const router = useRouter()
+const AdminLoginPage = () => {
+  const router = useRouter();
 
-    const loginForm = useForm<z.infer<typeof loginSchema>>({
-        resolver: zodResolver(loginSchema),
-        defaultValues: {
-            email: '',
-            password: ''
-        }
-    })
+  const loginForm = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    const gotToForgotPassword = () => {
-        router.push('/admin/forgot-password')
-    }
+  const gotToForgotPassword = () => {
+    router.push("/admin/forgot-password");
+  };
 
-    return (
-        <div className='flex flex-col justify-center items-center w-[500px] mx-auto h-full'>
-            <div className="headerSection text-left  w-full">
-                {/* main header */}
-                <p className="header ">
-                    Welcome Back
-                </p>
+  const handleGoToDashboard = (data: any) => {
+    console.log();
+    router.push("/private/admin/applications/overview");
+  };
 
-                <p className="description mt-2 text-gray-500">
-                    Enter your email and password to login
-                </p>
-            </div>
-            <Form {...loginForm}>
-                <form className='w-full flex flex-col gap-3 mt-10'>
-                    <InputFormField form={loginForm} name='email' type='email' label="Email" />
-                    <PasswordFormField form={loginForm} name='email' label="Password" />
+  return (
+    <div className="flex flex-col justify-center items-center w-[500px] mx-auto h-full">
+      <div className="headerSection text-left  w-full">
+        {/* main header */}
+        <p className="header ">Welcome Back</p>
 
-                    <ButtonLoading title='Login' />
-                </form>
-                <Button variant="link" className='self-end cursor-pointer' onClick={gotToForgotPassword}>Forgot Password</Button>
-            </Form>
+        <p className="description mt-2 text-gray-500">
+          Enter your email and password to login
+        </p>
+      </div>
+      <Form {...loginForm}>
+        <form
+          className="w-full flex flex-col gap-3 mt-10"
+          onSubmit={loginForm.handleSubmit(handleGoToDashboard)}
+        >
+          <InputFormField
+            form={loginForm}
+            name="email"
+            type="email"
+            label="Email"
+          />
+          <PasswordFormField form={loginForm} name="email" label="Password" />
 
+          <ButtonLoading
+            title="Login"
+            type="submit"
+            onClick={handleGoToDashboard}
+          />
+        </form>
+        <Button
+          variant="link"
+          className="self-end cursor-pointer"
+          onClick={gotToForgotPassword}
+        >
+          Forgot Password
+        </Button>
+      </Form>
 
-            {/* footer */}
-            <div className="footer mt-4">
-                <p className="text-sm text-gray-500 ">
-                    By signing in or creating an account, you agree to our{' '}
-                    <a href="/terms" className="text-blue-500 underline">
-                        Terms of Service
-                    </a>{' '}
-                    and{' '}
-                    <a href="/privacy" className="text-blue-500 underline">
-                        Privacy Policy
-                    </a>.
-                </p>
-            </div>
-        </div>
+      {/* footer */}
+      <div className="footer mt-4">
+        <p className="text-sm text-gray-500 ">
+          By signing in or creating an account, you agree to our{" "}
+          <a href="/terms" className="text-blue-500 underline">
+            Terms of Service
+          </a>{" "}
+          and{" "}
+          <a href="/privacy" className="text-blue-500 underline">
+            Privacy Policy
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default page
+export default AdminLoginPage;
